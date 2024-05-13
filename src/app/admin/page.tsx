ba53a46110685fea6c3eb1379ba7058558c954e2
afaqahmedkhan <afaq.qwe@gmail.com>
2024-05-13 11:58:26 +0200

    Add dashboard cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,41 @@
-export default function AdminDashboard(){
-    return <h1>Dashboard</h1>
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AdminDashboard() {
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard
+            title="Sales"
+            subtitle={` Orders`}
+            body={"Content"}
+        />
+        <DashboardCard
+            title="Customers"
+            subtitle={`Average Value`}
+            body={"Content"}
+        />
+        <DashboardCard
+            title="Active Products"
+            subtitle={`Inactive`}
+            body={"Content"}
+        />
+    </div>
+}
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string
+    body: string
+}
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{subtitle}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>{body}</p>
+            </CardContent>
+        </Card>
+    )
 }
